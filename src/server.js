295dca195270634fd71fedd408ac50567d225df0
Klement Omeri <klement-omeri97@hotmail.com>
2019-11-06 21:00:19 +0100

    refactoring server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/table-frontend'));
+app.use(express.static(__dirname + '/dist/table-frontend'));
 
-app.get('**', function (req, res) {
+app.get('/*', function (req, res) {
 
-    res.sendFile(path.join('./dist/table-frontend/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/table-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
