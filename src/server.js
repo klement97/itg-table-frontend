aadf75575635fe7de4b6bb9af0d5fa26c734139b
Klement Omeri <klement-omeri97@hotmail.com>
2019-11-06 21:42:10 +0100

    refactoring package.json

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.use(express.static('./dist/table-frontend'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join('/dist/table-frontend/index.html'));
+    res.sendFile(path.join('./dist/table-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
