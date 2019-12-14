34563bcc1d184a532380538faa76b794f6af252e
Klement Omeri <klement-omeri97@hotmail.com>
2019-12-14 14:44:18 +0100

    Remove Material imports from app module and run npm i

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,61 +11,29 @@ import {httpInterceptorProviders} from 'src/app/_interceptors';
 import {OrderModule} from 'src/app/order/order.module';
 import {AuthModule} from 'src/app/auth/auth.module';
 import {LayoutModule} from 'src/app/layout/layout.module';
-import * as MAT from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    BrowserModule,
-    OrderModule,
-    AuthModule,
-    LayoutModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true
-      }
-    }),
-    !environment.production ? StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}) : [],
-    MAT.MatCheckboxModule,
-    MAT.MatCheckboxModule,
-    MAT.MatButtonModule,
-    MAT.MatInputModule,
-    MAT.MatAutocompleteModule,
-    MAT.MatDatepickerModule,
-    MAT.MatFormFieldModule,
-    MAT.MatRadioModule,
-    MAT.MatSelectModule,
-    MAT.MatSliderModule,
-    MAT.MatSlideToggleModule,
-    MAT.MatMenuModule,
-    MAT.MatSidenavModule,
-    MAT.MatToolbarModule,
-    MAT.MatListModule,
-    MAT.MatGridListModule,
-    MAT.MatCardModule,
-    MAT.MatStepperModule,
-    MAT.MatTabsModule,
-    MAT.MatExpansionModule,
-    MAT.MatButtonToggleModule,
-    MAT.MatChipsModule,
-    MAT.MatIconModule,
-    MAT.MatProgressSpinnerModule,
-    MAT.MatProgressBarModule,
-    MAT.MatDialogModule,
-    MAT.MatTooltipModule,
-    MAT.MatSnackBarModule,
-    MAT.MatTableModule,
-    MAT.MatSortModule,
-    MAT.MatPaginatorModule,
-  ],
-  providers: [httpInterceptorProviders],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+	],
+	imports: [
+		BrowserAnimationsModule,
+		AppRoutingModule,
+		BrowserModule,
+		OrderModule,
+		AuthModule,
+		LayoutModule,
+		StoreModule.forRoot(reducers, {
+			metaReducers,
+			runtimeChecks: {
+				strictStateImmutability: true,
+				strictActionImmutability: true
+			}
+		}),
+		!environment.production ? StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}) : [],
+	],
+	providers: [httpInterceptorProviders],
+	bootstrap: [AppComponent]
 })
 export class AppModule {
 }
