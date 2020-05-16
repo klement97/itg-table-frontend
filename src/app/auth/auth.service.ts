94d9828fd3b12939c7c31a649b0c3d54d229195a
Klement Omeri <klementomeri97@gmail.com>
2020-05-16 16:16:15 +0200

    fix(auth): fix bug related to logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
 import {environment} from 'src/environments/environment';
 import {HttpClient} from '@angular/common/http';
-import {finalize, map} from 'rxjs/operators';
+import {map} from 'rxjs/operators';
 import {Router} from '@angular/router';
 import {CookieService} from 'ngx-cookie-service';
+
 
 const API = `${environment.apiHost}`;
 const AUTH = `${API}/auth`;
@@ -14,6 +15,7 @@ export const LOGIN = `${TOKEN}/login`;
 export const LOGOUT = `${TOKEN}/logout`;
 
 export const _TOKEN = 'token';
+
 
 @Injectable({
   providedIn: 'root'
@@ -30,22 +32,24 @@ export class AuthService {
 
   login(username, password) {
     return this.http.post(`${LOGIN}/`, {username, password})
-      .pipe(
-        map((token: { auth_token: string }) => {
-          if (token) {
-            this.cookieService.set(_TOKEN, token.auth_token, 365, '', '', false, 'Lax');
-            this.router.navigate(['order/tables']).then();
-            return token;
-          }
-        })
-      );
+               .pipe(
+                 map((token: { auth_token: string }) => {
+                   if (token) {
+                     this.cookieService.set(_TOKEN, token.auth_token, 365, '/', '', false, 'Strict');
+                     this.router.navigate(['order/tables']).then();
+                     return token;
+                   }
+                 })
+               );
   }
 
   logout() {
     const token: string = this.cookieService.get(_TOKEN);
     return this.http.post(`${LOGOUT}/`, token).pipe(
-      finalize(() => this.router.navigate(['/auth/login'])
-        .then(() => this.cookieService.delete(_TOKEN))
+      map(() => {
+          this.cookieService.delete(_TOKEN, '/');
+          this.router.navigate(['/auth/login']).then();
+        }
       ));
   }
 
